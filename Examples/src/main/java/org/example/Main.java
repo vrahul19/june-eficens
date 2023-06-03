44f229e97508693356073668934a6c28ae395d96
Rahul <vrahul1905@gmail.com>
2023-06-03 13:23:44 -0500

    My First Java Program setting up in GitHub Clone

--- Examples/src/main/java/org/example/Main.java
+++ Examples/src/main/java/org/example/Main.java
@@ -1,11 +1,7 @@
 package org.example;
 
-import OOPS.Vehicle;
 public class Main {
     public static void main(String[] args) {
-        Vehicle car = new Vehicle();
-        car.start();
-        car.stop();
-        //System.out.println("Hello world!");
+        System.out.println("My First Program!");
     }
 }
